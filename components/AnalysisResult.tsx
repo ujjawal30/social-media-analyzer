7722d7c4520a331932641595ac2b86da06c7a480
Ujjawal Gupta <ujjawalgupta1999.ug@gmail.com>
2025-01-05 19:41:57 +0530

    React markdown added

--- components/AnalysisResult.tsx
+++ components/AnalysisResult.tsx
@@ -7,9 +7,17 @@ interface AnalysisResultProps {
 
 const AnalysisResult: React.FC<AnalysisResultProps> = ({ result }) => {
   return (
-    <div className="mt-6 p-6 bg-gradient-to-r from-pink-500 via-red-500 to-yellow-500 rounded-2xl shadow-2xl text-white transform transition-all hover:scale-105 hover:shadow-3xl">
-      <h2 className="text-2xl font-bold mb-4">Analysis Result:</h2>
-      <ReactMarkdown>{result}</ReactMarkdown>
+    <div className="mt-6 p-6 bg-[#6633ee50] rounded-2xl shadow-2xl text-white transform transition-all overflow-auto max-h-[50vh] space-y-4">
+      <h2 className="text-2xl font-bold">{result ? "Analysis Result:" : "Unable to analyze the post. Please try again."}</h2>
+      <ReactMarkdown
+        className="prose text-white"
+        components={{
+          ul: ({ children }) => <ul className="list-disc list-inside">{children}</ul>,
+          h3: ({ children }) => <h3 className="text-lg font-semibold text-white">{children}</h3>,
+        }}
+      >
+        {result}
+      </ReactMarkdown>
     </div>
   );
 };
